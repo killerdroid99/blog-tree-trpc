d0af54e967aefd33f50394b275eb73dc03662930
killerdroid99 <ajempire09@gmail.com>
2022-09-03 15:59:20 +0530

    Fixed content overflow issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Navbar from "$/components/Navbar";
-import { trpc } from "../utils/trpc";
+import { trpc } from "$/utils/trpc";
 import Post from "$/components/Post";
+import AddPostButton from "$/components/AddPostButton";
 
 const Home: NextPage = () => {
 	const { data, isLoading, error, isError } = trpc.useQuery([
 		"posts.getAllPosts",
 	]);
+
 	return (
 		<>
 			<Head>
@@ -16,9 +18,10 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className="flex flex-col items-center pt-28 h-screen w-screen bg-neutral-800 text-slate-50 font-raleway">
+			<main className="flex flex-col items-center pt-28 h-screen w-screen bg-neutral-800 text-slate-50 font-raleway overflow-y-scroll">
 				<Navbar />
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[96vw] xl:w-[76vw]">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[96vw] xl:w-[76vw] relative">
+					<AddPostButton extraClassNames="fixed right-[1rem] xl:right-[12vw] top-[4rem]" />
 					{isLoading ? (
 						<div className="text-center w-full col-span-3">
 							Loading posts...
