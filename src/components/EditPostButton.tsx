b02188ae16f89da517f35748582148962d90f082
killerdroid99 <ajempire09@gmail.com>
2022-09-04 01:54:24 +0530

    Created some mock buttons for later use

---
+++ src/components/EditPostButton.tsx
@@ -0,0 +1,42 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+interface EditPostButtonProps {
+	extraClassNames?: string;
+}
+
+const EditPostButton = ({ extraClassNames }: EditPostButtonProps) => {
+	const { data: session } = useSession();
+
+	if (session) {
+		return (
+			<Link href="/add-post">
+				<button
+					className={`${extraClassNames} inline-flex items-center space-x-2 bg-orange-500 backdrop-blur-sm hover:bg-orange-600 text-sm font-bold py-1 px-3 rounded-full transition-all ease-in`}
+				>
+					<div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="w-5 h-5"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+							/>
+						</svg>
+					</div>
+					<span>Edit Post</span>
+				</button>
+			</Link>
+		);
+	}
+
+	return <></>;
+};
+
+export default EditPostButton;
