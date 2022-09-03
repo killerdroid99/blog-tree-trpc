9141be1b15cc6e8de60a4dd20d2bf98736471a44
killerdroid99 <ajempire09@gmail.com>
2022-09-03 15:57:59 +0530

    Created an add-post button

---
+++ src/components/AddPostButton.tsx
@@ -0,0 +1,42 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+interface AddPostButtonProps {
+	extraClassNames?: string;
+}
+
+const AddPostButton = ({ extraClassNames }: AddPostButtonProps) => {
+	const { data: session } = useSession();
+
+	if (session) {
+		return (
+			<Link href="/add-post">
+				<button
+					className={`${extraClassNames} inline-flex items-center space-x-2 bg-emerald-500 hover:bg-emerald-600 text-sm font-bold py-1 px-3 rounded-full transition-all ease-in`}
+				>
+					<div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="w-5 h-5"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+					</div>
+					<span>Add Post</span>
+				</button>
+			</Link>
+		);
+	}
+
+	return <></>;
+};
+
+export default AddPostButton;
