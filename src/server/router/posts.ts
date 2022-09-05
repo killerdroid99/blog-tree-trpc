abbc7d823e5c4d8ef035be05d9e7e1a3ef330752
killerdroid99 <ajempire09@gmail.com>
2022-09-05 16:01:01 +0530

    Added mutations for deletind and updating comments

--- src/server/router/posts.ts
+++ src/server/router/posts.ts
@@ -196,4 +196,56 @@ export const protectedPostRouter = createProtectedRouter()
 			});
 			return newComment;
 		},
+	})
+	.mutation("edit-comment", {
+		input: z.object({
+			body: z
+				.string()
+				.trim()
+				.min(5, "Comment body must be 5 or more characters long")
+				.max(500, "Comment body must be 500 or fewer characters long"),
+			id: z.string().min(1),
+		}),
+		async resolve({ ctx, input }) {
+			const newComment = await ctx.prisma.comments.update({
+				where: {
+					id: input.id,
+				},
+				data: {
+					body: input.body,
+				},
+			});
+			return newComment;
+		},
+	})
+	.mutation("delete-comment", {
+		input: z.object({
+			id: z.string().min(1),
+		}),
+		async resolve({ ctx, input }) {
+			const findComment = await ctx.prisma.comments.findUnique({
+				where: {
+					id: input.id,
+				},
+				include: {
+					post: {
+						select: {
+							userId: true,
+						},
+					},
+				},
+			});
+
+			if (
+				findComment?.userId === ctx.session.user.id ||
+				findComment?.post.userId === ctx.session.user.id
+			) {
+				const comment = await ctx.prisma.comments.delete({
+					where: {
+						id: input.id,
+					},
+				});
+				return comment;
+			}
+		},
 	});
